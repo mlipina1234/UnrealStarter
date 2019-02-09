f50cb5ada688de57dee68f89a7792933f6570fd4
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2019-02-09 18:02:32 -0500

    inital project creation

---
+++ Source/UnrealStarter/UnrealStarter.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealStarter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealStarter, "UnrealStarter" );
+ 
